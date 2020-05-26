2561f2ebf94001d77f4f33c371024edb9fcb26e7
mlo31415 <mlo@baskerville.org>
2020-05-26 12:38:36 -0400

    Add dialog to Git

---
+++ dlgEnterFancyName.py
@@ -0,0 +1,14 @@
+from GenDlgEnterFancyName import dlgEnterFancyName
+
+#####################################################################################
+class dlgEnterFancyNameWindow(dlgEnterFancyName):
+    def __init__(self, parent):
+        dlgEnterFancyName.__init__(self, parent)
+        self._FancyName: str=""
+        self.ShowModal()
+
+    def OnBuCreateConSeries(self, event):
+        self.Hide()
+
+    def OnTextChanged(self, event):
+        self._FancyName=self.m_textCtrl4.Value
