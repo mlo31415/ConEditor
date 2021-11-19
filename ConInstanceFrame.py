2c0c95277b270380a01d13a007764deb00220c6b
mlo31415 <mlo@baskerville.org>
2021-11-18 21:46:25 -0500

    Drop useless code

--- ConInstanceFrame.py
+++ ConInstanceFrame.py
@@ -314,7 +314,6 @@ class ConInstanceDialogClass(GenConInstanceFrame):
 
 
         # Read in the template
-        file=None
         try:
             Log("sys.path[0]=  "+sys.path[0])
             Log("sys.argv[0]=  "+sys.argv[0])
