6bc3ebef5e31bfaa016ef56468bdbb430485e8f1
mlo31415 <mlo@baskerville.org>
2021-11-26 14:02:17 -0500

    Use the right kind of slash for newline

--- ConEditorFrame.py
+++ ConEditorFrame.py
@@ -284,7 +284,7 @@ class ConEditorFrame(GenConEditorFrame):
         newtable+='  <tbody>\n'
         for i, row in enumerate(self.Datasource.Rows):
             if i == 3:  # Add a crude horizontal rule between items 3 (Misc. Cons) and 4 (1st real con)
-                newtable+="    <tr>\n      <td>------------------</td>/n    <tr>\n"
+                newtable+="    <tr>\n      <td>------------------</td>\n    <tr>\n"
             newtable+="    <tr>\n"
             newtable+='      <td>'+FormatLink(row.URL, row.Name)+'</td>\n'
             newtable+="    </tr>\n"
